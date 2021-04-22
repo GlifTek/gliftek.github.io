758915c003ef3ca8c2bc6d25abff852a006969e0
GlifTek <49026094+GlifTek@users.noreply.github.com>
2021-04-22 18:38:19 -0400

    Add files via upload

--- assets/js/modal_popup_images.js
+++ assets/js/modal_popup_images.js
@@ -17,17 +17,15 @@ function modalFunction(imgName)
     // Get the image and insert it inside the modal - use its "alt" text as a caption
     var img = document.getElementById('img_' + imgName);
     var modalImg = document.getElementById('modal_' + imgName);
-    // var captionText = document.getElementById(captionVar);
+    var captionText = document.getElementById('caption_' + imgName);
 
-    // clicks(modal,img,modalImg,captionText);
     img.onclick = function(){
         modal.style.display = "block";
         modalImg.src = this.src;
-        // captionText.innerHTML = this.alt;
+        captionText.innerHTML = this.alt;
     }
 
     // Get the <span> element that closes the modal
-    // var span = document.getElementsByClassName("close")[0];
     var span = document.getElementById('close_' + imgName);
 
     // When the user clicks on <span> (x), close the modal
