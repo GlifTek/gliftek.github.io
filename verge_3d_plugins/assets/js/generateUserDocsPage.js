eacbbae37a197ae9580cd1abb2fb4d7c6a5b40e2
GlifTek <49026094+GlifTek@users.noreply.github.com>
2021-07-19 18:45:12 -0700

    Add files via upload

--- verge_3d_plugins/assets/js/generateUserDocsPage.js
+++ verge_3d_plugins/assets/js/generateUserDocsPage.js
@@ -10,6 +10,7 @@ function set_Globals()
   window.page = (new URLSearchParams(window.location.search)).get('page');
   console.log('page:',page);
 
+  //  MAKE THIS 'IF PAGE ISN'T ANY EXISTING PAGE THEN GO TO MAIN PAGE'
   if ( page == 'main_menu' )
   {
     makeMainMenuPage();
@@ -60,6 +61,8 @@ function set_Globals()
   LIST_images_example = [];
   LIST_images_console = [];
   LIST_images_extra = [];
+
+  loadMainJS();
 
   loadJSON();
 
@@ -322,7 +325,7 @@ function make_site_HTML()
 
   
 
-  setTimeout(doEndScript(), 500);
+  // setTimeout(doEndScript(), 500);
 
   function doEndScript()
   {
@@ -330,26 +333,25 @@ function make_site_HTML()
     let endScript = document.createElement('script');
     
     endScript.crossOrigin = 'anonymous';
-    endScript.src="./assets/js/main.js";
-    // endScript.src="./main.js";
+    endScript.src="./assets/js/main.js";    
     
-    
-    
+    document.head.appendChild(endScript);   
+
     endScript.onload = function () {
 
-      console.log('MAIN.JS loaded!')
-      //do stuff with the script
-  };
+    console.log('MAIN.JS loaded!');
 
-  document.head.appendChild(endScript);   
-
-
-    // var script = document.createElement('script');
+    let mainCSS = document.createElement('style');
     
-// script.src = something;
+    mainCSS.crossOrigin = 'anonymous';
+    mainCSS.src="./assets/css/main.css";
+    document.head.appendChild(mainCSS);
 
-// document.head.appendChild(script); //or something of the likes
+    mainCSS.onload = function () {
+      console.log('MAIN.CSS loaded!')
+    };
 
+  };
 
 
 
@@ -805,10 +807,26 @@ function set_intro_HTML()
 
   }
 
+  let pluginNameandIntro, userDocumentationOrNot;
+
+  if ( page == 'featured' )
+  {
+    pluginNameandIntro = pluginPack.intro;
+    userDocumentationOrNot = 'Plugin Packs'
+  }
+
+  else if ( page !== 'featured' )
+  {    
+    pluginNameandIntro = `The [ ${pluginPack_title_UpperCase} ] Plugin Pack ${pluginPack.intro}`;
+    userDocumentationOrNot = 'User Documentation';
+  }
+
+
+
   let result =
   `<header class="major" style='font-style: italic'>
-  <span class="date">User Documentation</span>
-  The [ ${pluginPack_title_UpperCase} ] Plugin Pack ${pluginPack.intro}
+  <span class="date">${userDocumentationOrNot}</span>
+  ${pluginNameandIntro}
   </header>
   <hr>
   <div style='text-align: left; font-style: italic' >
@@ -863,6 +881,7 @@ function set_post_HTML( pluginName, pic )
 
   let pluginName_NoUnderscores = pluginName.replace(/_/g, ' ');;
   let pluginName_UpperCase = pluginName_NoUnderscores.toUpperCase();
+  let pluginName_LowerCase = pluginName.toLowerCase();
 
   let divPuzzlePic = 'div_puzzle_' + pic;
   let divExamplePic = 'div_example_' + pic;
@@ -881,6 +900,8 @@ function set_post_HTML( pluginName, pic )
   let consoleText = pluginPackEvaled.consoleText;
   let extraText = pluginPackEvaled.extraText;
   let customHTML = pluginPackEvaled.customHTML;
+
+
 
 
 
@@ -937,6 +958,39 @@ function set_post_HTML( pluginName, pic )
 
 
 
+  let mainText;
+  if ( pluginPackEvaled.mainText )
+  {
+    mainText = pluginPackEvaled.mainText;
+  }
+  else if ( !pluginPackEvaled.mainText )
+  { mainText = '' }
+
+
+  let mainHTML;
+
+  if ( mainText == '' || mainText == 'undefined' )
+  {
+    mainHTML = '';
+  }
+  else if ( mainText !== '' || mainText !== 'undefined' )
+  {
+    mainHTML = 
+    `<ul class="actions special">
+      <li>
+        <a href="https://gliftek.gumroad.com" 
+        target="_blank" rel="noreferrer noopener"
+        class="button large">
+        Buy Now
+        </a>
+        <br/>
+        <br/>
+        <a href="user_docs.html?page=${pluginName_LowerCase}"
+        class="button large">User Docs</a>
+      </li>
+    </ul>`
+  }
+
 
   let extraHTML;
 
@@ -954,7 +1008,7 @@ function set_post_HTML( pluginName, pic )
 
   };
 
-  
+
 
 
   let result =
@@ -975,6 +1029,8 @@ function set_post_HTML( pluginName, pic )
       ${extraHTML}
 
       ${customHTML}
+
+      ${mainHTML}
 
       <div style="width: 100%; height: 1px; background: #ffffff;"></div>
 
@@ -1107,3 +1163,267 @@ function copyright_HTML()
 
 
 };  //  END makePluginPage()
+
+
+
+
+function loadMainJS()
+{
+  //  FROM: Massively by HTML5 UP
+	//  html5up.net | @ajlkn
+
+  (function($) {
+
+    var	$window = $(window),
+      $body = $('body'),
+      $wrapper = $('#wrapper'),
+      $header = $('#header'),
+      $nav = $('#nav'),
+      $main = $('#main'),
+      $navPanelToggle, $navPanel, $navPanelInner;
+  
+    // Breakpoints.
+      breakpoints({
+        default:   ['1681px',   null       ],
+        xlarge:    ['1281px',   '1680px'   ],
+        large:     ['981px',    '1280px'   ],
+        medium:    ['737px',    '980px'    ],
+        small:     ['481px',    '736px'    ],
+        xsmall:    ['361px',    '480px'    ],
+        xxsmall:   [null,       '360px'    ]
+      });
+  
+    /**
+     * Applies parallax scrolling to an element's background image.
+     * @return {jQuery} jQuery object.
+     */
+    $.fn._parallax = function(intensity) {
+  
+      var	$window = $(window),
+        $this = $(this);
+  
+      if (this.length == 0 || intensity === 0)
+        return $this;
+  
+      if (this.length > 1) {
+  
+        for (var i=0; i < this.length; i++)
+          $(this[i])._parallax(intensity);
+  
+        return $this;
+  
+      }
+  
+      if (!intensity)
+        intensity = 0.25;
+  
+      $this.each(function() {
+  
+        var $t = $(this),
+          $bg = $('<div class="bg"></div>').appendTo($t),
+          on, off;
+  
+        on = function() {
+  
+          $bg
+            .removeClass('fixed')
+            .css('transform', 'matrix(1,0,0,1,0,0)');
+  
+          $window
+            .on('scroll._parallax', function() {
+  
+              var pos = parseInt($window.scrollTop()) - parseInt($t.position().top);
+  
+              $bg.css('transform', 'matrix(1,0,0,1,0,' + (pos * intensity) + ')');
+  
+            });
+  
+        };
+  
+        off = function() {
+  
+          $bg
+            .addClass('fixed')
+            .css('transform', 'none');
+  
+          $window
+            .off('scroll._parallax');
+  
+        };
+  
+        // Disable parallax on ..
+          if (browser.name == 'ie'			// IE
+          ||	browser.name == 'edge'			// Edge
+          ||	window.devicePixelRatio > 1		// Retina/HiDPI (= poor performance)
+          ||	browser.mobile)					// Mobile devices
+            off();
+  
+        // Enable everywhere else.
+          else {
+  
+            breakpoints.on('>large', on);
+            breakpoints.on('<=large', off);
+  
+          }
+  
+      });
+  
+      $window
+        .off('load._parallax resize._parallax')
+        .on('load._parallax resize._parallax', function() {
+          $window.trigger('scroll');
+        });
+  
+      return $(this);
+  
+    };
+  
+    // Play initial animations on page load.
+      $window.on('load', function() {
+        window.setTimeout(function() {
+          $body.removeClass('is-preload');
+        }, 100);
+      });
+  
+    // Scrolly.
+      $('.scrolly').scrolly();
+  
+    // Background.
+      $wrapper._parallax(0.925);
+  
+    // Nav Panel.
+  
+      // Toggle.
+        $navPanelToggle = $(
+          '<a href="#navPanel" id="navPanelToggle">Menu</a>'
+        )
+          .appendTo($wrapper);
+  
+        // Change toggle styling once we've scrolled past the header.
+          $header.scrollex({
+            bottom: '5vh',
+            enter: function() {
+              $navPanelToggle.removeClass('alt');
+            },
+            leave: function() {
+              $navPanelToggle.addClass('alt');
+            }
+          });
+  
+      // Panel.
+        $navPanel = $(
+          '<div id="navPanel">' +
+            '<nav>' +
+            '</nav>' +
+            '<a href="#navPanel" class="close"></a>' +
+          '</div>'
+        )
+          .appendTo($body)
+          .panel({
+            delay: 500,
+            hideOnClick: true,
+            hideOnSwipe: true,
+            resetScroll: true,
+            resetForms: true,
+            side: 'right',
+            target: $body,
+            visibleClass: 'is-navPanel-visible'
+          });
+  
+          // console.log('setting up main.js');
+  
+          // Get inner.
+          $navPanelInner = $navPanel.children('nav');
+  
+        // Move nav content on breakpoint change.
+          var $navContent = $nav.children();
+  
+          breakpoints.on('>medium', function() {
+  
+            // NavPanel -> Nav.
+              $navContent.appendTo($nav);
+  
+            // Flip icon classes.
+              $nav.find('.icons, .icon')
+                .removeClass('alt');
+  
+          });
+  
+          breakpoints.on('<=medium', function() {
+  
+            // Nav -> NavPanel.
+              $navContent.appendTo($navPanelInner);
+  
+            // Flip icon classes.
+              $navPanelInner.find('.icons, .icon')
+                .addClass('alt');
+  
+          });
+  
+        // Hack: Disable transitions on WP.
+          if (browser.os == 'wp'
+          &&	browser.osVersion < 10)
+            $navPanel
+              .css('transition', 'none');
+  
+    // Intro.
+      var $intro = $('#intro');
+  
+      if ($intro.length > 0) {
+  
+        // Hack: Fix flex min-height on IE.
+          if (browser.name == 'ie') {
+            $window.on('resize.ie-intro-fix', function() {
+  
+              var h = $intro.height();
+  
+              if (h > $window.height())
+                $intro.css('height', 'auto');
+              else
+                $intro.css('height', h);
+  
+            }).trigger('resize.ie-intro-fix');
+          }
+  
+        // Hide intro on scroll (> small).
+          breakpoints.on('>small', function() {
+  
+            $main.unscrollex();
+  
+            $main.scrollex({
+              mode: 'bottom',
+              top: '25vh',
+              bottom: '-50vh',
+              enter: function() {
+                $intro.addClass('hidden');
+              },
+              leave: function() {
+                $intro.removeClass('hidden');
+              }
+            });
+  
+          });
+  
+        // Hide intro on scroll (<= small).
+          breakpoints.on('<=small', function() {
+  
+            $main.unscrollex();
+  
+            $main.scrollex({
+              mode: 'middle',
+              top: '15vh',
+              bottom: '-15vh',
+              enter: function() {
+                $intro.addClass('hidden');
+              },
+              leave: function() {
+                $intro.removeClass('hidden');
+              }
+            });
+  
+        });
+  
+      }
+  
+  })(jQuery);
+}
